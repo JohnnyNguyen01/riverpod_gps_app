16f70aa75d63b7443771af845612eace211ed13f
johnny <johnnynguyenwork@gmail.com>
2021-06-03 14:58:28 +1000

    current progress

--- lib/states/user_state_notifier.dart
+++ lib/states/user_state_notifier.dart
@@ -3,23 +3,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pet_tracker_youtube/device/device.dart';
 import 'package:pet_tracker_youtube/domain/models/models.dart';
 
-abstract class UserState {
-  const UserState();
-}
-
-class UserInitial extends UserState {
-  const UserInitial();
-}
-
-class UserSigningIn extends UserState {
-  const UserSigningIn();
-}
-
-class UserSignedIn extends UserState {
-  final User user;
-  const UserSignedIn(this.user);
-}
-
 final userStateProvider = StateNotifierProvider<UserStateNotifier, User>((ref) {
   final gpsRepo = ref.read(deviceGpsRepoProvider);
   return UserStateNotifier(gpsRepo: gpsRepo);
@@ -28,16 +11,13 @@ final userStateProvider = StateNotifierProvider<UserStateNotifier, User>((ref) {
 class UserStateNotifier extends StateNotifier<User> {
   final DeviceGps _gpsRepo;
 
-  //todo refactor below
   UserStateNotifier({required DeviceGps gpsRepo})
       : _gpsRepo = gpsRepo,
         super(const User(email: "", userName: "", location: LatLng(0, 0)));
 
   //todo: utilise loading state as well
   void loginuser({required String email, required String name}) {
-    final user =
-        User(email: email, userName: name, location: const LatLng(0, 0));
-    state = user;
+    state = User(email: email, userName: name, location: const LatLng(0, 0));
   }
 
   ///Set the user's location [LatLng] to their current location.
@@ -47,6 +27,7 @@ class UserStateNotifier extends StateNotifier<User> {
       state = state.copyWith(
           location:
               LatLng(currentLocation.latitude, currentLocation.longitude));
+      print("current user is ${state.toString()}");
     } catch (e) {
       throw Failure(code: "", message: e.toString());
     }
