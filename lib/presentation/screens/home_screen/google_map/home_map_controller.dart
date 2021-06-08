e9ffe19e30f1d5a3774351ee1a26ed1ad729d5a8
johnny <johnnynguyenwork@gmail.com>
2021-06-08 17:11:28 +1000

    Camera now animates according to device and user positions

--- lib/presentation/screens/home_screen/google_map/home_map_controller.dart
+++ lib/presentation/screens/home_screen/google_map/home_map_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pet_tracker_youtube/device/device.dart';
 
 /*
  * Controller Provider 
@@ -23,9 +24,21 @@ class HomeMapController {
   Completer<GoogleMapController> get googleMapController =>
       _googleMapController;
 
+  ///Animates the Google Map Camera to the specified location,
+  ///
+  ///`target` - the position we want to animate to.
   Future<void> setCameraToNewPosition({required LatLng target}) async {
     final mapController = await _googleMapController.future;
     final newPosition = CameraPosition(target: target, zoom: 14.476);
     mapController.animateCamera(CameraUpdate.newCameraPosition(newPosition));
+  }
+
+  ///Animates the Google Map Camerat to the User's current device location.
+  Future<void> setCameraTouserLocation() async {
+    final deviceRepo = _read(deviceGpsRepoProvider);
+    final devicePosition = await deviceRepo.getDevicePosition();
+    final deviceLatLng =
+        LatLng(devicePosition.latitude, devicePosition.longitude);
+    await setCameraToNewPosition(target: deviceLatLng);
   }
 }
--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -1,11 +1,9 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pet_tracker_youtube/presentation/screens/home_screen/google_map/home_map_controller.dart';
 import 'package:pet_tracker_youtube/presentation/screens/home_screen/home_screen_controller.dart';
 import 'package:pet_tracker_youtube/presentation/screens/home_screen/widgets/custom_drawer.dart';
 import 'package:pet_tracker_youtube/presentation/screens/home_screen/widgets/pet_card.dart';
-import 'package:pet_tracker_youtube/states/stream_providers/pet_coordinate_stream_provider.dart';
 import 'google_map/home_map.dart';
 
 // ignore: use_key_in_widget_constructors
@@ -32,8 +30,9 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: const CustomDrawer(),
       body: Stack(children: [
         HomeMap(),
-        PetCard(),
-        _BuildNavDrawerButton(),
+        const PetCard(),
+        const _BuildNavDrawerButton(),
+        const _BuildCurentPositionButton()
       ]),
     );
   }
@@ -49,6 +48,31 @@ class _BuildNavDrawerButton extends StatelessWidget {
       child: IconButton(
         icon: const Icon(Icons.menu),
         onPressed: () => Scaffold.of(context).openDrawer(),
+      ),
+    );
+  }
+}
+
+/*
+ * Current position button 
+ */
+class _BuildCurentPositionButton extends StatelessWidget {
+  const _BuildCurentPositionButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 5,
+      bottom: 200,
+      child: Consumer(
+        builder: (context, watch, child) => FloatingActionButton.extended(
+          onPressed: () async => await context
+              .read(homeMapControllerProvider)
+              .setCameraTouserLocation(),
+          label: const Icon(Icons.my_location),
+          shape: const CircleBorder(),
+          backgroundColor: const Color(0xFF8ACAC0),
+        ),
       ),
     );
   }
