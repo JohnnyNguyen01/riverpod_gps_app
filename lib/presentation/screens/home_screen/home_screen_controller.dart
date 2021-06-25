52215890ecabb87fc532a3679f0d4aa9acec1468
johnny <johnnynguyenwork@gmail.com>
2021-06-25 12:32:48 +1000

    Notifications now returned if tarzan's outside geofence every 60 seconds

--- lib/presentation/screens/home_screen/home_screen_controller.dart
+++ lib/presentation/screens/home_screen/home_screen_controller.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pet_tracker_youtube/device/device.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_tracker_youtube/states/geofence_notifier.dart';
